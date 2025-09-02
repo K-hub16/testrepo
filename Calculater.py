d371afb73dbbd7aedc622fd6f0ebe7cdfacb42e9
K-hub16 <kaustubhpalkar0216@gmail.com>
2025-09-02 22:53:23 +0530

    Calculater

---
+++ Calculater.py
@@ -0,0 +1,23 @@
+# Operators
+#Python has different types of operators as follows
+
+#Addition
+print(2+9)
+
+#Substraction
+print(9-3)
+
+#Multiplication
+print(8*9)
+
+#Division
+print(15/7)
+
+#Exponential
+print(3**3)
+
+#modulous
+print(5%3)
+
+#Floor Division
+print(15//7)
